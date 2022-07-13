3083eec0bc64dc38b7588948e118db6e46959445
Victor Matias <fvictor1705@gmail.com>
2022-07-13 17:11:25 -0300

    Remove console.log da função reduceDataItem.

--- script.js
+++ script.js
@@ -19,8 +19,6 @@ const reduceDataItem = async (id) => { // Promise!
     name: data.title,
     salePrice: data.price,
   };
-
-  // console.log(obj);
 };
 
 const createProductImageElement = (imageSource) => {
